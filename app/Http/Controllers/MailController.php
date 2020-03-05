a5c27d88d9b797ef9d43e97ba6d7597551e57faf
Iqbalaulia <16410100012@stikom.edu>
2020-03-05 08:51:29 +0700

    project exploration about google gmail

---
+++ app/Http/Controllers/MailController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Mail;
+use Session;
+use App\Mail\SendEmail;
+
+class MailController extends Controller
+{
+    public function home(){
+     return view('mail.home');   
+    }
+
+    public function sendmail(Request $request){
+
+        $this->validate($request,[
+            "email"     =>  "required",
+            "subject"   =>  "required",
+            "message"   =>  "required"
+        ]);
+
+        $email      =   $request->email;
+        $subject    =   $request->subject;
+        $message    =   $request->message;
+
+        Mail::to($email)->send(new SendEmail($subject,$message));
+        Session::flash("success");
+        return back();
+    }
+}
---
+++ resources/views/mail/home.blade.php
@@ -0,0 +1,56 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-10">
+            <div class="card">
+                <div class="card-header bg-primary text-white">My Test Email</div>
+
+                <div class="card-body">
+                    <form method="POST" action="{{ url('send/email') }}">
+                        @csrf
+                        
+                        @if (Session::has("success"))
+                            <div class="alert alert-success">
+                                <b>Successfull</b>, your email sent.
+                            </div>
+                        @endif
+
+                        <div class="form-group row">
+                            <label for="subject" class="col-md-4 col-form-label text-md-right">Subject</label>
+
+                            <div class="col-md-6">
+                                <input id="subject" type="text" class="form-control @error('subject') is-invalid @enderror" name="subject" value="{{ old('subject') }}" required autocomplete="subject" autofocus>
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="subject" class="col-md-4 col-form-label text-md-right">Email</label>
+
+                            <div class="col-md-6">
+                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="message" class="col-md-4 col-form-label text-md-right">Message</label>
+
+                            <div class="col-md-6">
+                                <textarea class="form-control" name="message" id="" cols="30" rows="5"></textarea>
+                            </div>
+                        </div>
+                       
+
+                        <div class="form-group row mb-0">
+                            <div class="col-md-8 offset-md-4">
+                                <button type="submit" class="btn btn-success">Send Email</button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
